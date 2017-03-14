c34fca264792e08a104606de53e01222429bc143
Marciocha <taleja555@interia.pl>
2017-03-14 19:40:15 +0100

    add final files

---
+++ src/com/sdajava/problem_wydawania_reszty/Main.java
@@ -0,0 +1,52 @@
+package com.sdajava.problem_wydawania_reszty;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj kwote, ktora chcesz wydac: ");
+
+        int R = scanner.nextInt();
+
+
+        int[] N = {200, 100, 50, 20, 10, 5, 2, 1};
+        int L;
+
+        for (int i = 0; i<N.length; i++){
+            if(R < 0){
+                break;
+            }
+            else if(R >= N[i]){
+                L = R / N[i];
+                R = R - (N[i] * L);
+                System.out.println("Nominał " + N[i] + " x " + L);
+            }else{
+            }
+        }
+    }
+}
+
+        //lub
+
+    /*Scanner scanner = new Scanner(System.in);
+      System.out.println("Podaj kwote, ktora chcesz wydac: ");
+
+                int R = scanner.nextInt();
+
+
+                int[] N = {200, 100, 50, 20, 10, 5, 2, 1};
+                int i = 0;
+
+
+                while(R > 0) {
+                    if (R >= N[i]) {
+                        int L = R / N[i];
+                        R = R - (N[i] * L);
+                        System.out.println("Nominał " + N[i] + " x " + L);
+                    } else {
+                        i = i + 1;
+                    }
+                }*/
